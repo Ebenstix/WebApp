5be2a79255b92e3bbad218ef3c74be81d35a114d
Ebenezer <55884751+Ebenstix@users.noreply.github.com>
2024-10-30 20:45:22 +0100

    Update wsgi.py

--- multimedia_site/multimedia_site/wsgi.py
+++ multimedia_site/multimedia_site/wsgi.py
@@ -1,13 +1,8 @@
-"""
-WSGI config for multimedia_site project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
 
 import os
+import sys
+
+sys.path.insert(0, 'multimedia_site')
 
 from django.core.wsgi import get_wsgi_application
 
