2b5a2c88c796d4060b6f2f5271b2b7c5b5a7dc03
Ebenezer <55884751+Ebenstix@users.noreply.github.com>
2024-10-30 20:55:32 +0100

    Update wsgi.py

--- multimedia_site/multimedia_site/wsgi.py
+++ multimedia_site/multimedia_site/wsgi.py
@@ -1,11 +1,9 @@
 
 import os
 import sys
-
-sys.path.insert(0, 'multimedia_site')
-
 from django.core.wsgi import get_wsgi_application
 
+sys.path.insert(0, 'multimedia_site')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multimedia_site.settings')
 
 application = get_wsgi_application()
